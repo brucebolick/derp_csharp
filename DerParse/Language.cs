b1fbd0be5466ab50fce5d16f5cf10dc82d238a8b
Bruce Bolick <bbolick@1800contacts.com>
2015-09-04 17:00:03 -0600

    yolo

---
+++ DerParse/Language.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DerParse
+{
+    public abstract class Language
+    {
+        public abstract Lazy<bool> Nullable();
+        public abstract Lazy<Language> Derive(char inputCharacter);
+
+        public static Lazy<Language> Create(Func<Language> function)
+        {
+            return new Lazy<Language>(function);
+        }
+    }
+}
